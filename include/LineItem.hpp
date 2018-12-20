796f6dc1310d25b8d6d90db48b9f03d406ea6b76
lwesterl <lauri.v.westerholm@gmail.com>
2018-12-20 21:52:36 +0200

    LineItem.cpp and added isInside function

--- include/LineItem.hpp
+++ include/LineItem.hpp
@@ -10,6 +10,8 @@
 /*    INCLUDES    */
 
 #include <QGraphicsLineItem>
+#include <QDebug>
+#include <cmath>
 
 
 /**
@@ -22,6 +24,8 @@ class LineItem : public QGraphicsLineItem
 {
   public:
 
+    static const int CompensationValue; /**< Value used to compensate clicking the item to make it easier */
+
   /**
     *   @brief Constructor for LineItem
     *   @param x1 Start x coordinate
@@ -29,46 +33,56 @@ class LineItem : public QGraphicsLineItem
     *   @param x2 End x coordinate
     *   @param y2 End y coordinate
     */
-    LineItem(unsigned x1, unsigned y1, unsigned x2, unsigned y2):QGraphicsLineItem(x1, y1, x2, y2),
-    x1(x1), x2(x2), y1(y1), y2(y2) {}
-
+    LineItem(unsigned x1, unsigned y1, unsigned x2, unsigned y2);
   /**
     *   @brief Get start x coordinate
     *   @return Returns x1
     */
-    unsigned getX1() {return x1;}
+    unsigned getX1();
 
     /**
       *   @brief Get end x coordinate
       *   @return Returns x2
       */
-    unsigned getX2() {return x2;}
+    unsigned getX2();
 
     /**
       *   @brief Get start y coordinate
       *   @return Returns y1
       */
-    unsigned getY1() {return y1;}
+    unsigned getY1();
 
     /**
       *   @brief Get end y coordinate
       *   @return Returns y2
       */
-    unsigned getY2() {return y2;}
+    unsigned getY2();
 
     /**
       *   @brief Update end x and y coordinates
       *   @param x_new New end x coordinate
       *   @param y_new New end y coordinate
       */
-    void updatePosition(unsigned x_new, unsigned y_new)
-    {
-      x2 = x_new;
-      y2 = y_new;
-    }
+    void updatePosition(unsigned x_new, unsigned y_new);
+
+    /**
+      *   @brief Check if coordinate is inside the item
+      *   @param x x coordinate
+      *   @param y y coordinate
+      *   @return Returns true if the coordinate is inside the item
+      */
+    bool isInside(unsigned x, unsigned y);
+
+    /**
+      *   @brief Calculate angle
+      *   @remark Angle is between world x-axis and item x-axis
+      */
+    void calculateAngle();
+
   private:
     unsigned x1;
     unsigned x2;
     unsigned y1;
     unsigned y2;
+    float angle; /**< angle between world x-axis and item x-axis */
 };
---
+++ src/LineItem.cpp
@@ -0,0 +1,122 @@
+/**
+  *   @file LineItem.cpp
+  *   @brief Source code for LineItem class
+  */
+
+#include "../include/LineItem.hpp"
+
+/*  Class LineItem */
+
+const int LineItem::CompensationValue = 10;
+
+LineItem::LineItem(unsigned x1, unsigned y1, unsigned x2, unsigned y2):QGraphicsLineItem(x1, y1, x2, y2),
+x1(x1), x2(x2), y1(y1), y2(y2)
+{
+  // calculate angle between world x-axis and item x-axis (use arctan)
+  calculateAngle();
+}
+
+unsigned LineItem::getX1()
+{
+  return x1;
+}
+
+unsigned LineItem::getX2()
+{
+  return x2;
+}
+
+unsigned LineItem::getY1()
+{
+  return y1;
+}
+
+unsigned LineItem::getY2()
+{
+  return y2;
+}
+
+void LineItem::updatePosition(unsigned x_new, unsigned y_new)
+{
+  x2 = x_new;
+  y2 = y_new;
+  // update angle between world x-axis and item x-axis (use arctan)
+  calculateAngle();
+}
+
+bool LineItem::isInside(unsigned x, unsigned y)
+{
+  if (x2 >= x1)
+  {
+    if ( (static_cast<int>(x1 - CompensationValue / 2) <= static_cast<int>(x)) &&
+          (static_cast<int>(x) <= static_cast<int>(x2 + CompensationValue /2)) )
+    {
+      // x is inside the item, check y using tan function and angle
+      float y_corr;
+      if (y2 > y1)
+      {
+        // line goes downwards
+        y_corr = std::tan(angle) * static_cast<float>(x - x1) + static_cast<float> (y1);
+      }
+      else
+      {
+        // line goes upwards
+        y_corr = - std::tan(angle) * static_cast<float>(x - x1) + static_cast<float> (y1);
+      }
+      if ( (static_cast<int>(y) > static_cast<int>(y_corr - CompensationValue)) &&
+          (static_cast<int>(y) < static_cast<int>(y_corr + CompensationValue)) )
+          {
+            // coordinate is inside item (if compensation is taken into account)
+            return true;
+          }
+    }
+  }
+  else
+  {
+    if ( (static_cast<int>(x2 - CompensationValue / 2) <= static_cast<int>(x)) &&
+          (static_cast<int>(x) <= static_cast<int>(x1 + CompensationValue /2)) )
+    {
+      // x is inside the item, check y using tan function and angle
+      float y_corr;
+      if (y2 > y1)
+      {
+        // line goes upwards (but starts from x2)
+        y_corr = - std::tan(angle) * static_cast<float>(x - x2) + static_cast<float> (y2);
+      }
+      else
+      {
+        // line goes downwards (starts from x2). Now angle has to be changed to the adjacent angle
+        y_corr = - std::tan(M_PI - angle) * static_cast<float>(x1 - x) + static_cast<float> (y1);
+      }
+      if ( (static_cast<int>(y) > static_cast<int>(y_corr - CompensationValue)) &&
+          (static_cast<int>(y) < static_cast<int>(y_corr + CompensationValue)) )
+          {
+            // coordinate is inside item (if compensation is taken into account)
+            return true;
+          }
+
+    }
+  }
+
+  return false;
+}
+
+void LineItem::calculateAngle()
+{
+  if (y2 > y1 && x2 >= x1)
+  {
+    angle = std::atan(static_cast<float>(y2 - y1) / static_cast<float>(x2 - x1));
+  }
+  else if (y2 <= y1 && x2 >= x1)
+  {
+    angle = std::atan(static_cast<float>(y1 - y2) / static_cast<float>(x2 - x1));
+  }
+  else if (y2 > y1 && x2 < x1)
+  {
+    angle = std::atan(static_cast<float>(y2 - y1) / static_cast<float>(x1 - x2));
+  }
+  else
+  {
+    angle = M_PI - std::atan(static_cast<float>(y1 - y2) / static_cast<float>(x1 - x2));
+  }
+}
