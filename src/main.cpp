41c6dfdc02758e77881d371ca4394e1662efdefd
lwesterl <lauri.v.westerholm@gmail.com>
2018-12-22 21:39:38 +0200

    Added app_icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 
 /**
-  *   @file GUI_test.cpp
+  *   @file main.cpp
   *   @author Lauri Westerholm
-  *   @brief Test main for Editor program
+  *   @brief Main for Editor program
   */
 
 
 
 #include "../include/GUI.hpp"
+#include <QIcon>
 
 /**
   *   @brief Simple main for Editor program
@@ -18,6 +19,9 @@
 int main(int args, char *argv[])
 {
   QApplication app(args, argv);
+  QIcon app_icon;
+  app_icon.addFile(Path::Images[Path::ImageName::app_icon]);
+  app.setWindowIcon(app_icon);
   GUI gui;
   gui.resize(700,500);
   gui.show();
